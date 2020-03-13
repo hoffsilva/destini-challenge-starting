bb8b2670cc85c635a1eafbedecca03588d48cd23
Hoff Silva <hoff.henry@gmail.com>
2020-03-12 22:22:35 -0300

    //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -39,15 +39,15 @@ class StoryBrain {
 ];
 
 String getStory() {
-  return _storyData.first.storyTitle;
+  return _storyData[storyNumber].storyTitle;
 }
 
 String getChoice1() {
-  return _storyData.first.choice1;
+  return _storyData[storyNumber].choice1;
 }
 
 String getChoice2() {
-  return _storyData.first.choice2;
+  return _storyData[storyNumber].choice2;
 }
 
 void nextStory(int choiceNumber) {
@@ -83,6 +83,6 @@ void restart() {
 
 //TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
